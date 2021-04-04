68baab6a1ceb5ce75197aa744748b8491e3b424c
hex <n7f@protonmail.com>
2021-04-04 17:51:12 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./public/**/*.html",
+    "./src/**/*.{js,vue}"
+  ],
+  darkMode: "media", // or false or "class"
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
